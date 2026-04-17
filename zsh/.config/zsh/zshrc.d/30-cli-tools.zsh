48c254f915ec6f0d87e5ee3a24791db1ab8b0b4d
ae2rs <lucasdc@amo.co>
2026-04-17 08:48:17 +0200

    zsh: fzf exact mode

--- zsh/.config/zsh/zshrc.d/30-cli-tools.zsh
+++ zsh/.config/zsh/zshrc.d/30-cli-tools.zsh
@@ -1,4 +1,8 @@
 # --- FZF ---
+# Make Ctrl-R history search match exact terms instead of loose subsequences.
+if [[ " ${FZF_CTRL_R_OPTS-} " != *" --exact "* ]]; then
+  export FZF_CTRL_R_OPTS="${FZF_CTRL_R_OPTS:+${FZF_CTRL_R_OPTS} }--exact"
+fi
 source <(fzf --zsh)
 
 # --- OrbStack ---
@@ -9,5 +13,3 @@ alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 
 # --- Zoxide ---
 eval "$(zoxide init zsh)"
-
-
